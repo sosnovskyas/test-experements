8cfbae648dd9e2a6b129f74baaf12060d43fcb1d
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2017-08-11 00:16:02 +0300

    uncommented source

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-// export default class First {
-//     pow(a: number, b: number): number {
-//         return a * b;
-//     }
-// }
-//
-// const first = new First();
-//
-// console.log(first.pow(2, 3));
+export class First {
+    pow(a: number, b: number): number {
+        return a * b;
+    }
+}
+
+const first = new First();
+
+console.log(first.pow(2, 3));
