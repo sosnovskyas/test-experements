5b01be9026659a89a0edcf9057709643ce4015a2
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2017-08-11 00:15:51 +0300

    modified to real test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,14 @@
 import {expect} from 'chai';
 import 'mocha';
 
+import {First} from './index';
+
 describe('Hello function', () => {
     it('should return hello world', () => {
         expect(1).equal(1);
     });
-    it('should return hello world', () => {
-        expect(1).equal(2);
+    it('POW (2, 3)= 6', () => {
+        const first = new First();
+        expect(first.pow(2,3)).equal(6);
     });
 });
