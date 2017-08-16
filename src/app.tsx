54e4059b4d467793958dd0c3e29c676a1bc397f5
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2017-08-16 09:18:28 +0300

    added root element for mounting

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/hello";
+import {Hello} from "./components/hello";
+
+const root = document.createElement('div.root');
+document.body.appendChild(root);
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    <Hello compiler="TypeScript" framework="React"/>,
+    root
 );
